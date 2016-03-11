ac6b9bdeb07994cdd0b17c6f0cc201ddf4668e12
jiimms <jiimms@yahoo.com>
2016-03-11 17:39:30 +0530

    first commit

---
+++ ruby6.4_quiz.rb
@@ -0,0 +1,27 @@
+Quiz
+
+			Take the Quiz
+			1) Conditionals can be denoted in single line using 
+					a) Ternary operator (correct)
+					b) Logical Operator 
+					c) Arithmetic Operator 
+
+			2)Which are loops in Ruby 
+					a) for loop,each loop 
+					b) while loop,loop 
+					c) both (correct)
+
+			3)Ranges are used in 
+					a) while loop 
+					b) case statement (correct)
+					c) none 
+
+			4)Unless condition is used as an alternate of 
+					a) for loop 
+					b) if condition (correct)
+					c) Procedure  
+
+			5)Until is used as an alternate for 
+					a) For 
+					b) Each 
+					c) While  (correct)
